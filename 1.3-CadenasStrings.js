2c4b761339aa586ae7f022cae30dee19b18d4b71
Alex <alexx.mozam@gmail.com>
2020-09-20 11:17:43 -0500

    Propiedades y métodos de String

--- 1.3-CadenasStrings.js
+++ 1.3-CadenasStrings.js
@@ -1,4 +1,27 @@
-// Declarar variable
+// Declarar variable y utilizar propiedad length
 let name = "Alejandro";
-//  imprimir valor y utilizar propiedad length
-console.log(name.length);
+console.log(`Tu nombre es ${name}`);
+console.log("el número de caracteres de tu nombre son", name.length);
+
+// object y uso propiedades .toUpperCase .toLowerCase
+
+const user = {
+  name: "alex",
+  mail: "alexx.mozam@gmail.com",
+};
+
+console.log(
+  user.name.toLowerCase(),
+
+  user.mail.toLocaleUpperCase()
+);
+
+// validar que no se repita una palabra
+let lorem =
+  "Lorem ipsum dolor sit amet amet consectetur adipisicing elit. Nostrum vel dolores";
+
+console.log(lorem.includes("amet"));
+
+// Me mandan una cadena de texto con los datos de una usuaria, genero un array
+let = userA = "Ana Jimenez ana@gmail.com RH";
+console.log(userA.split(" "));
