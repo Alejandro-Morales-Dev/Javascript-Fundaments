85686a6bebcc8dbfe9cdc446bc1ad84a4a388bc3
Alex <alexx.mozam@gmail.com>
2020-09-20 12:48:26 -0500

    Se agrega tema de templates string (interpolar) y tipos de datos numéricos con el método parse INT, FLOAT

---
+++ 1.3-TemplateString.js
@@ -0,0 +1,13 @@
+let name = "Alex";
+let age = 29;
+
+let user = `Tu nombres es ${name} y tienes ${age} años`;
+console.log(user);
+
+// Estaciones formato Html con Template string
+let ul = `
+<ul>
+    <li>Products</li>
+    <li>Contact</li>
+</ul>`;
+console.log(ul);
---
+++ 1.4-Numbers.js
@@ -0,0 +1,11 @@
+// Método tofixed() - números de decimales a mostrar
+let pi = 3.141616253458;
+console.log(pi.toFixed(4));
+// función saber que tipo de dato
+console.log(typeof pi);
+
+// Método parseInt
+let n = "5";
+let n2 = 5;
+let R = n + n2;
+console.log(parseInt(n) + n2); // realizar un casting método parseInt
